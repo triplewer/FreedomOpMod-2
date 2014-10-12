c8a05e75b316eae1dbd750c5e7a9a3b319507846
Tyler <ultrahostmc@gmail.com>
2014-10-11 22:36:11 -0400

    FreedomOpMod V1.0
    * Ethan and Buildcarter8s version of fopm

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_fopm.java
@@ -0,0 +1,40 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.Config.TFM_ConfigEntry;
+import me.StevenLawson.TotalFreedomMod.Config.TFM_MainConfig;
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import me.StevenLawson.TotalFreedomMod.TFM_BanManager;
+import me.StevenLawson.TotalFreedomMod.TFM_CommandBlocker;
+import me.StevenLawson.TotalFreedomMod.TFM_Log;
+import me.StevenLawson.TotalFreedomMod.TFM_PermbanList;
+import me.StevenLawson.TotalFreedomMod.TFM_PlayerList;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
+@CommandParameters(description = "Shows information about FreedomOpMod", usage = "/<command> ")
+public class Command_tfm extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length == 1)
+        {
+        playerMsg("FreedomOpMod for 'FreedomOp', the Massive all-op server.", ChatColor.GOLD);
+        playerMsg(String.format("Version "
+                + ChatColor.BLUE + "%s.%s" + ChatColor.GOLD + ", built "
+                + ChatColor.BLUE + "%s" + ChatColor.GOLD + " by "
+                + ChatColor.BLUE + "%s" + ChatColor.GOLD + ".",
+                TotalFreedomMod.pluginVersion,
+                TotalFreedomMod.buildNumber,
+                TotalFreedomMod.buildDate,
+                TotalFreedomMod.buildCreator), ChatColor.GOLD);
+        playerMsg("Running on " + TFM_ConfigEntry.SERVER_NAME.getString() + ".", ChatColor.GOLD);
+        playerMsg("Created by buildcarter8 & EthanPlayz", ChatColor.GOLD);
+
+        return true;
+    }
+}
