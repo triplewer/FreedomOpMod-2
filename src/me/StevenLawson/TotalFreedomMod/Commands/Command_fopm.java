11caea1f2c48ab1072ec20bbc7d0dca61efba2e8
Tyler <buildcarter8@users.noreply.github.com>
2014-10-11 22:42:38 -0400

    Added CMD shit

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_fopm.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_fopm.java
@@ -16,7 +16,7 @@ import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
 @CommandParameters(description = "Shows information about FreedomOpMod", usage = "/<command> ")
-public class Command_tfm extends TFM_Command
+public class Command_fopm extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
