dc03f35f62e57bee579b23632812fbd69ce1189f
Ethan <ethanplayz1356@gmail.com>
2014-10-14 18:16:56 -0400

    Update Command_ai.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_ai.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_ai.java
@@ -14,11 +14,10 @@ public class Command_ai extends TFM_Command
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
         
-            playerMsg(ChatColor.AQUA + "The following is accurate as of 10/07/14");
+            
             playerMsg(ChatColor.GREEN + "To apply for admin you need to go to the forums at http://freedomop.boards.net");
-            playerMsg(ChatColor.YELLOW + "Then read the requirements in the 'admin applications' board.");
-            playerMsg(ChatColor.WHITE + "Then if you feel you are ready, make a new thread in the 'admin applications'' board.");
-            playerMsg(ChatColor.BLUE + "And fill out the template in the new thread.");
+            playerMsg(ChatColor.YELLOW + "Then go to the board 'Super Admin Applications' and read the thread labeled 'CrafterSmith12's super admin application - EXAMPLE!.");
+            playerMsg(ChatColor.PURPLE + "Then if your ready to make a admin application, go and hit CREATE THREAD, and follow the questions!.");
             playerMsg(ChatColor.RED + "We ask for you not to ask existing admins for recommendations, this will get your application denied.");
             playerMsg(ChatColor.GOLD + "Good Luck!");
             return true;
